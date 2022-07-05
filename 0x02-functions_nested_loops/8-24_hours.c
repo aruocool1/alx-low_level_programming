d72c6691cd05376905e7a4dcf24a5724e6d3070e
aruocool1 <adjainoe@gmail.com>
2022-07-05 14:08:16 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ int h, m;
 for (h = 0; h < 24; h++)
 {
 for (m = 0; m < 60; m++)
-{								{
+{
 _putchar((h / 10) + '0');
 _putchar((h % 10) + '0');
 _putchar(':');
@@ -22,4 +22,5 @@ _putchar((m % 10) + '0');
 _putchar('\n');
 }
 }
+return (0);
 }
