24296ca9a18424b527d67950153814af0dc3428e
aruocool1 <adjainoe@gmail.com>
2022-07-05 17:29:06 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,7 +13,7 @@ k = 2;
 printf("%lu", j);
 for (i = 1; i < 91; i++)
 {
-printf(",%lu", k);
+printf(", %lu", k);
 k = k + j;
 j = k - j;
 }
@@ -28,7 +28,7 @@ printf("%lu", k2 % 1000000000);
 k1 = k1 + j1;
 j1 = k1 - j1;
 k2 = k2 + j2;
-j2 = k2 - j2;							
+j2 = k2 - j2;
 }
 printf("\n");
 return (0);
