b8bd09be50e04127fee3beac876069911ff5177f
aruocool1 <adjainoe@gmail.com>
2022-07-15 09:02:28 -0700

    changes

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,11 +7,33 @@
 
 void print_number(int n)
 {
-unsigned int n1;
+unsigned int tens, dig, pos = n;
+double temp_tens = 1;
 
+if(n == 0)
+_putchar('0');
+else
+{
 if (n < 0)
 {
-n1 = -n;
+pos = n * -1;
+_putchar('-');
+}
+while (temp_tens <= pos)
+temp_tens *= 10;
+tens = temp_tens /10;
+
+while (tens >= 1)
+{
+dig = pos /tens;
+_putchar(dig + '0');
+pos = (pos - (tens * dig ));
+tens /= 10;
+}
+}
+}
+
+/*
 _putchar('-');
 } else
 {
@@ -23,3 +45,4 @@ print_number(n1 / 10);
 
 _putchar((n1 % 10) + '0');
 }
+*/
