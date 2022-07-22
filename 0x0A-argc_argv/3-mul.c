b2797d355a7bc7f7dc955d039ab8310d75e21864
aruocool1 <adjainoe@gmail.com>
2022-07-22 16:07:51 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,6 @@
 * Return: If the program receives two arguments - 0.
 * If the program does not receive two arguments - 1.
 */
-
 int main(int argc, char *argv[])
 {
 int num1, num2, prod;
@@ -20,6 +19,9 @@ return (1);
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 prod = num1 * num2;
+
 printf("%d\n", prod);
+
 return (0);
+
 }
